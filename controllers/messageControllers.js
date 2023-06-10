91fc4ce2d175ec2f1e69d896a7b0213c3cd9c243
Rishu jha <jharishu796@gmail>
2023-06-10 18:10:35 +0530

    bloggy backend

---
+++ controllers/messageControllers.js
@@ -0,0 +1,59 @@
+const { MessageModel } = require("../models/messageModel");
+
+const sendMessage = async (req, res, next) => {
+  const chatId = req.params.chatId;
+  const { message } = req.body;
+  if (!chatId || !message) {
+    let error = new Error("please provide all the fields");
+    return next(error);
+  }
+  try {
+    let newMessage = new MessageModel({
+      chatId,
+      sender: req.user._id,
+      message,
+    });
+    let savedMessage = await newMessage.save();
+    try {
+      savedMessage = await savedMessage.populate("sender", "-password");
+      savedMessage = await savedMessage.populate("chatId");
+      res.json({
+        success: true,
+        message: "message sent",
+        result: savedMessage,
+      });
+    } catch (err) {
+      console.log("error occured while populating savedMessage");
+      next(err);
+    }
+  } catch (err) {
+    console.log(
+      "error occured while sending/saving message->messageController"
+    );
+    next(err);
+  }
+};
+
+const getMessages = async (req, res, next) => {
+  const { chatId } = req.params;
+  try {
+    let message = await MessageModel.find({ chatId }).populate(
+      "sender",
+      "-password"
+    ).populate("chatId");
+    ;
+    res.json({
+      success: true,
+      message: "fetched all messages",
+      result: message,
+    });
+  } catch (err) {
+    console.log("error occured while fetching message in messageController",err);
+    next(err);
+  }
+};
+
+module.exports = {
+  sendMessage,
+  getMessages,
+};
---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const cors = require("cors");
+// const { errorHandler } = require("./middlewares/errorHandler");
+
+//connection with mongodb database;
+const { connection } = require("./config/db");
+//routes
+const { userRouter } = require("./routes/userRoutes");
+const { chatRouter } = require("./routes/chatRoutes");
+const { messageRoutes } = require("./routes/messageRoutes");
+const { blogRoutes } = require("./routes/blogRoutes");
+const { authMiddleware } = require("./middlewares/authMiddleware");
+// const { use } = require("bcrypt/promises");
+
+const app = express();
+
+// app.use(
+//   cookieParser(null, {
+//     // domain:"localhost"
+//     domain: `${process.env.DOMAIN}`,
+//   })
+// );
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: `https://bloggy-cyan.vercel.app/`,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
+app.use(express.json());
+
+app.get("/", authMiddleware, (req, res) => {
+  // console.log(req.user);
+  res.json({ message: "working" });
+});
+
+//routes
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRoutes);
+app.use("/api/blogs",blogRoutes);
+
+//for handling invalid routes -> 404 Not Found
+app.all("*", (req, res, next) => {
+  let error = { message: `404 Not Found: ${req.url}`, statusCode: 404 };
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "something went wrong";
+  res.status(statusCode).json({
+    status: statusCode,
+    message,
+  });
+});
+
+const httpServer = app.listen(8080, async () => {
+  console.log("server is running...");
+  try {
+    await connection;
+    console.log("connected to database");
+  } catch (err) {
+    console.log(err);
+    console.log("Not connected to database");
+  }
+});
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: `${process.env.FRONT_END_URL}`,
+    credential: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  // console.log(socket);
+  console.log("connected to sockets");
+  socket.on("setup", (userId) => {
+    socket.join(userId);
+    socket.emit("connected");
+  });
+
+  //join chat/room
+  socket.on("join chat", (chatId) => {
+    socket.join(chatId);
+  });
+
+  socket.on("new message", (newMessageData) => {
+    // console.log("new message called", newMessageData.message);
+    let chat = newMessageData?.chatId;
+    if (!chat?.users) return;
+    // console.log("yes")
+    chat.users.forEach((elem) => {
+      if (elem != newMessageData.sender._id) {
+        socket.in(elem).emit("arrived", newMessageData);
+      }
+    });
+  });
+});
